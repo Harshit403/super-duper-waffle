3f408a83dbdeb521893c717a0b9a11d149b317be
Harshit Shrivastav <hshrivastav100@gmail.com>
2024-12-27 21:21:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ async def save_plan(course_id: str, name: str = Form(...), pdf: UploadFile = Fil
     
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=PORT)
+    uvicorn.run(app, host="0.0.0.0", port=18931)
